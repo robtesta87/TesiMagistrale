dcfa3f6944aaef54b13b0e10cef9747564443a25
Roberto Testa <roberto.testa87@gmail.com>
2015-07-28 16:09:56 +0200

    extract mantions

---
+++ src/ExtractorMentions/ExtractorFirstMentions.java
@@ -0,0 +1,16 @@
+package ExtractorMentions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExtractorFirstMentions {
+	public Map<String,String> ExtractMentions (String text){
+		
+		Map<String,String> wikiIdToText = new HashMap<String, String>();
+		
+		return
+	}
+	public static void main(String[] args) {
+
+	}
+}
