44c73a8e43bfab3f29faf23a8ed0d52ec6ace2d8
Roberto Testa <roberto.testa87@gmail.com>
2015-07-30 15:00:40 +0200

    inserimento metodo separazione frasi in un testo

---
+++ src/test/NERDemo.java
@@ -0,0 +1,129 @@
+package test;
+
+import edu.stanford.nlp.ie.AbstractSequenceClassifier;
+import edu.stanford.nlp.ie.crf.*;
+import edu.stanford.nlp.io.IOUtils;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.sequences.DocumentReaderAndWriter;
+import edu.stanford.nlp.util.Triple;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/** This is a demo of calling CRFClassifier programmatically.
+ *  <p>
+ *  Usage: {@code java -mx400m -cp "*" NERDemo [serializedClassifier [fileName]] }
+ *  <p>
+ *  If arguments aren't specified, they default to
+ *  classifiers/english.all.3class.distsim.crf.ser.gz and some hardcoded sample text.
+ *  If run with arguments, it shows some of the ways to get k-best labelings and
+ *  probabilities out with CRFClassifier. If run without arguments, it shows some of
+ *  the alternative output formats that you can get.
+ *  <p>
+ *  To use CRFClassifier from the command line:
+ *  </p><blockquote>
+ *  {@code java -mx400m edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier [classifier] -textFile [file] }
+ *  </blockquote><p>
+ *  Or if the file is already tokenized and one word per line, perhaps in
+ *  a tab-separated value format with extra columns for part-of-speech tag,
+ *  etc., use the version below (note the 's' instead of the 'x'):
+ *  </p><blockquote>
+ *  {@code java -mx400m edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier [classifier] -testFile [file] }
+ *  </blockquote>
+ *
+ *  @author Jenny Finkel
+ *  @author Christopher Manning
+ */
+
+public class NERDemo {
+	
+	public List<String> analyzeThisFile(String path){
+		List<String> annotationsNE = new ArrayList<String>();
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(path));
+			
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return annotationsNE;
+		
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		String serializedClassifier = "classifiers/english.conll.4class.distsim.crf.ser.gz";
+
+		AbstractSequenceClassifier<CoreLabel> classifier = CRFClassifier.getClassifier(serializedClassifier);
+
+		String[]example ={"Alberta (/ælˈbɜrtə/) is a western province of Canada. With a population of 3,645,257 in 2011 and an estimated population of 4,145,992 as of October 1, 2014, it is Canada's fourth-most populous province and most populous of Canada's three prairie provinces. Alberta and its neighbour, Saskatchewan, were established as provinces on September 1, 1905.The current premier of the province is Rachel Notley."};
+		for (String str : example) {
+			System.out.println(classifier.classifyToString(str));
+		}
+		System.out.println("---");
+
+		for (String str : example) {
+			// This one puts in spaces and newlines between tokens, so just print not println.
+			System.out.print(classifier.classifyToString(str, "slashTags", false));
+		}
+		System.out.println("---");
+
+		for (String str : example) {
+			// This one is best for dealing with the output as a TSV (tab-separated column) file.
+			// The first column gives entities, the second their classes, and the third the remaining text in a document
+			System.out.print(classifier.classifyToString(str, "tabbedEntities", false));
+		}
+		System.out.println("---");
+
+		for (String str : example) {
+			System.out.println(classifier.classifyWithInlineXML(str));
+		}
+		System.out.println("---");
+
+		for (String str : example) {
+			System.out.println(classifier.classifyToString(str, "xml", true));
+		}
+		System.out.println("---");
+
+		for (String str : example) {
+			System.out.print(classifier.classifyToString(str, "tsv", false));
+		}
+		System.out.println("---");
+
+		// This gets out entities with character offsets
+		int j = 0;
+		for (String str : example) {
+			j++;
+			List<Triple<String,Integer,Integer>> triples = classifier.classifyToCharacterOffsets(str);
+			for (Triple<String,Integer,Integer> trip : triples) {
+				System.out.printf("String %s is type of %s over character offsets [%d, %d) in sentence %d.%n",str.substring(trip.second(), trip.third()),trip.first(), trip.second(), trip.third, j);
+			}
+		}
+		System.out.println("---");
+
+		// This prints out all the details of what is stored for each token
+		int i=0;
+		for (String str : example) {
+			for (List<CoreLabel> lcl : classifier.classify(str)) {
+				for (CoreLabel cl : lcl) {
+					System.out.print(i++ + ": ");
+					System.out.println(cl.toShorterString());
+				}
+			}
+		}
+
+		System.out.println("---");
+
+	}
+
+
+}
--- src/test/testCoreNLP2.java
+++ src/test/testCoreNLP2.java
@@ -25,7 +25,7 @@ public class testCoreNLP2 {
 		//String text = "As an anti-dogmatic philosophy, anarchism draws on many currents of thought and strategy. Anarchism does not offer a fixed body of doctrine from a single particular world view, instead fluxing and flowing as a philosophy.[19] There are many types and traditions of anarchism, not all of which are mutually exclusive.[20] Anarchist schools of thought can differ fundamentally, supporting anything from extreme individualism to complete collectivism.[10] Strains of anarchism have often been divided into the categories of social and individualist anarchism or similar dual classifications.[21][22] Anarchism is usually considered a radical left-wing ideology,[23][24] and much of anarchist economics and anarchist legal philosophy reflect anti-authoritarian interpretations of communism, collectivism, syndicalism, mutualism, or participatory economics. Bill Clinton was born in Italy.";
 		String text = "Back to the Future is a 1985 American comic science fiction film directed by Robert Zemeckis, written by Zemeckis and Bob Gale, produced by Gale and Neil Canton, and stars Michael J. Fox, Christopher Lloyd, Lea Thompson, Crispin Glover and Thomas F. Wilson. Steven Spielberg, Kathleen Kennedy, and Frank Marshall served as executive producers. In the film, teenager Marty McFly (Fox) is sent back in time to 1955, where he meets his future parents in high school and accidentally becomes his mother's romantic interest. Marty must repair the damage to history by causing his parents-to-be to fall in love, and with the help of eccentric scientist Dr. Emmett \"Doc\" Brown (Lloyd), he must find a way to return to 1985.";
 				// create an empty Annotation just with the given text
-				Annotation document = new Annotation(text);
+		Annotation document = new Annotation(text);
 
 		// run all Annotators on this text
 		pipeline.annotate(document);
@@ -40,14 +40,16 @@ public class testCoreNLP2 {
 		        // this is the text of the token
 		        String word = token.get(TextAnnotation.class);
 		        
-		        System.out.println("Word: "+word);
+		        //System.out.println("Word: "+word);
 		        // this is the POS tag of the token
 		        String pos = token.get(PartOfSpeechAnnotation.class);
 		        // this is the NER label of the token
 		        String ne = token.get(NamedEntityTagAnnotation.class);   
-		        System.out.println("WORD: "+word+" pos: "+pos+" NER: "+ne);
+		        //System.out.println("WORD: "+word+" pos: "+pos+" NER: "+ne);
 		      }
 			cont++;
 		}
 		}
+	
+	
 	}
