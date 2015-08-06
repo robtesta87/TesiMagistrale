7c0d81c57d9b29348f1a2e905e3a9a50dce7d4aa
Roberto Testa <roberto.testa87@gmail.com>
2015-08-06 16:40:14 +0200

    problema risolto delle sostituzione mention mid

--- src/ExtractorMentions/ExtractorFirstMentions.java
+++ src/ExtractorMentions/ExtractorFirstMentions.java
@@ -343,9 +343,6 @@ public  class ExtractorFirstMentions {
 
 	public static void main(String[] args) {
 
-
-
-
 		String title = "Dracula";
 		//String mentionWiki = "This is  [[Hello world | Hello world]] hgjkgy [[ciao mondo | Hello world]]dskdasldlsa [[pippo]]hgsajhkgc[[A#sfhfdhws|dfhask of A]] AC Milan";
 		String mentionWiki = "The story of [[Dracula]] [[Bram Stoker]] has been the basis for numerous films and plays. Stoker himself wrote the first theatrical adaptation, which was presented at the Lyceum Theatre under the title ''Dracula, or The Undead'' shortly before the novel's publication and performed only once. Popular films include ''[[Dracula (1931 English-language film)|Dracula]]'' (1931), ''[[Dracula (1958 film)|Dracula]]'' (alternative title: ''The Horror of Dracula'') (1958), and ''[[Dracula (1992 film)|Dracula]]'' (also known as ''Bram Stoker's Dracula'') (1992). ''Dracula'' was also adapted as ''[[Nosferatu]]'' (1922), a film directed by the German director [[F. W. Murnau]], without permission from Stoker's widow; the filmmakers attempted to avoid copyright problems by altering many of the details, including changing the name of the villain to \"[[Count Orlok]]\". AC Milan [[Bill Clinton]]. Clinton was born in Italy.";
